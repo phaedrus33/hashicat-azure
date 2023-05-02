fa7e0e8fe32a3c3e90190b8847df2719f2cabc2f
Jose Rodriguez <joser33usa@gmail.com>
2023-05-02 18:39:25 +0000

    Added remote backend

--- variables.tf
+++ variables.tf
@@ -10,11 +10,12 @@
 
 variable "prefix" {
   description = "This prefix will be included in the name of most resources."
+  default = "jrodriguez"
 }
 
 variable "location" {
   description = "The region where the virtual network is created."
-  default     = "centralus"
+  default     = "westus"
 }
 
 variable "address_space" {
